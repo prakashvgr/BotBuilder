2a62e5c1165f3a1060ad06ef2f41441b1e8cb6ef
Prakash Rajendran <prakash.rajendran@capgemini.com>
2017-02-21 13:58:25 +0530

    Updated the server listinging port

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ var builder = require('botbuilder');
 
 var server = restify.createServer();
 
-server.listen( process.env.port || process.env.PORT || 3978, function() {
+server.listen( process.env.port || process.env.PORT || 8080, function() {
 	console.log('%s Listinging to %s', server.name, server.url);
 });
 
-var MICROSOFT_APP_ID = process.env.MICROSOFT_APP_ID || '1e834cae-0d7e-4c99-b7da-ab55bdd5d704';
-var MICROSOFT_APP_PASSWORD = process.env.MICROSOFT_APP_PASSWORD || 'noYRUzVyHwgPCGDFgD2FXAb';
-
 var connector = new builder.ChatConnector({
-	appID: MICROSOFT_APP_ID,
-	appPassword: MICROSOFT_APP_PASSWORD
+	appID: process.env.MICROSOFT_APP_ID,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 var bot = new builder.UniversalBot(connector);
